2accdc8e8988ae346fec1b6878939ec0d1c3aa95
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-06-23 15:40:48 -0500

    Add feedback within app and plant data from firebase
    * docs: Update instructions for android simulation, update scripts
    
    * feat: Bottom navigation on Homescreen, with link to shopping site
    
    * fix: fixes #11 padding bug on horizontal scroll
    
    * chore: move styles to style object
    
    * chore: Add minify start script
    
    * feat: Add feedback from within the app
    
    * chore: remove unused Settings screen
    
    * chore: Add Async Storage package from rn community @1.7.1
    
    * feat: Updated screen for adding a plant
    
    * chore: Help links updated with better error handling
    
    * feat: Update data source for plants and add ability to remove and rename plants from within the care guide
    
    * chore: Remove unused My Care Guides screen
    
    * chore: Add dev mode for key debugging on Settings and error handling
    
    * chore: Set and check for version on loading screen, clear data if unavailable
    
    * chore: Add and use refresh parameter on homescreen
    
    * chore: Minor styling updates
    
    * chore: set current version when auth changes and user exists
    
    * chore: better error handling when something goes wrong during download
    
    * chore: update left padding on my plants
    
    * chore: Update data and sources to reflect new naming of plants -> species
    
    * chore: replace webbrowser with link and remove webbrowser dep
    
    * chore: Add scripts for publishing, remove bg image on welcome screen

--- src/Home/PersonalHome.js
+++ src/Home/PersonalHome.js
@@ -4,14 +4,15 @@ import {
   View,
   ScrollView,
   RefreshControl,
+  Linking,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
 import {
-  Touchable, Type, DetailHeader, Media, Button,
+  Touchable, Type, DetailHeader, Media, Button, ScreenWithBottomNav,
 } from '../shared/components';
 import {
-  space, COLORS, hitSlop,
+  space, COLORS, hitSlop, LINKS,
 } from '../shared/constants';
 import { useAuth } from '../shared/use-auth';
 import handleError from '../shared/data/handleError';
@@ -21,7 +22,10 @@ import { getSavedZipcode } from './data';
 
 const styles = StyleSheet.create({
   container: {
-    padding: space[2],
+    paddingVertical: space[2],
+  },
+  sidePadding: {
+    paddingHorizontal: space[2],
   },
   weatherSection: {
     marginBottom: space[3],
@@ -38,19 +42,32 @@ const styles = StyleSheet.create({
     marginBottom: space[2],
   },
   plantTitles: {
-    alignItems: 'baseline'
+    alignItems: 'baseline',
+    paddingHorizontal: space[2],
   },
   infoBox: {
     backgroundColor: COLORS.blueTint,
-    padding: space[3],
+    padding: space[4],
     marginVertical: space[2],
   },
   infoBoxText: {
     marginBottom: space[2],
+  },
+  feedbackSection: {
+    paddingHorizontal: space[2],
+    paddingVertical: space[3],
+  },
+  feedbackBodyWrapper: {
+    paddingTop: space[1],
+    paddingBottom: space[2],
+  },
+  bottomNavWrapper: {
+    backgroundColor: COLORS.magenta,
+    padding: space[1],
   }
 });
 
-const PersonalHome = (props) => {
+const PersonalHome = ({ navigation }) => {
   const [zipcode, setZipcode] = useState(null);
   const [loadingZipcode, setLoadingZipcode] = useState(false);
   const [reloadPlantsToggle, setReloadPlantsToggle] = useState(1);
@@ -69,64 +86,108 @@ const PersonalHome = (props) => {
       });
   };
 
-  const handleRefresh = () => {
-    loadZipcode();
+  const refreshPlants = () => {
     setReloadPlantsToggle(reloadPlantsToggle * -1);
   };
+
+  const handleRefresh = () => {
+    loadZipcode();
+    refreshPlants();
+  };
+
+  const handleAddPlantPress = () => {
+    navigation.navigate('AddCareGuide', {
+      onGoBack: () => handleRefresh(),
+    });
+  };
+
+  const handleFeedbackPress = () => {
+    Linking
+      .openURL(LINKS.feedbackForm)
+      .catch(() => {});
+  };
+
+  useEffect(() => {
+    if (
+      navigation.getParam('refresh', false) === true
+    ) {
+      refreshPlants();
+    }
+  }, [navigation]);
 
   useEffect(() => {
     loadZipcode();
   }, []);
 
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl refreshing={loadingZipcode} onRefresh={handleRefresh} />
-      }
-    >
-      <View style={styles.container}>
-        <View style={styles.weatherSection}>
-          <DetailHeader weight="bold">What&apos;s it like outside?</DetailHeader>
-          {zipcode || loadingZipcode ? (
-            <Weather zipcode={zipcode} loadingZipcode={loadingZipcode} />
-          ) : (
-            <View style={styles.infoBox}>
-              <View style={styles.infoBoxText}>
-                <Type>
-                  We can&apos;t show you the kind of weather your plants will
-                   experience because you don&apos;t have your zipcode set.
-                </Type>
-              </View>
-              <Button onPress="Settings">Add your Zipcode</Button>
+    <ScreenWithBottomNav current="home">
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={loadingZipcode} onRefresh={handleRefresh} />
+        }
+      >
+        <View style={styles.container}>
+          <View style={styles.weatherSection}>
+            <View style={styles.sidePadding}>
+              <DetailHeader weight="bold">What&apos;s it like outside?</DetailHeader>
             </View>
-          )}
+            {zipcode || loadingZipcode ? (
+              <View style={{ paddingVertical: space[2] }}>
+                <Weather zipcode={zipcode} loadingZipcode={loadingZipcode} />
+              </View>
+            ) : (
+              <View style={styles.infoBox}>
+                <View style={styles.infoBoxText}>
+                  <Type>
+                We can&apos;t show you the kind of weather your plants will
+                 experience because you don&apos;t have your zipcode set.
+                  </Type>
+                </View>
+                <Button onPress="Settings">Add your Zipcode</Button>
+              </View>
+            )}
 
+          </View>
+          <View style={styles.plantsSection}>
+            <Media direction="row" style={styles.plantTitles}>
+              <Media.Body>
+                <DetailHeader weight="bold">My plants</DetailHeader>
+              </Media.Body>
+              <Media.Item>
+                <Touchable onPress={handleAddPlantPress} hitSlop={hitSlop}>
+                  <Type color="grass" weight="bold">Add a plant</Type>
+                </Touchable>
+              </Media.Item>
+            </Media>
+            <MySavedPlants
+              onAddPlant={handleAddPlantPress}
+              reloadToggle={reloadPlantsToggle}
+              navigate={navigation.navigate}
+              signout={auth.signout}
+              style={{ paddingLeft: space[2] }}
+            />
+          </View>
+          <View style={styles.feedbackSection}>
+            <DetailHeader weight="bold">How can we do better?</DetailHeader>
+            <View style={styles.feedbackBodyWrapper}>
+              <Type>
+              We are trying to make the best #@$& app out there for growing your
+               own food, so we want to hear from our community! What do you
+                like? What can we do better?
+              </Type>
+            </View>
+            <Button onPress={handleFeedbackPress}>Tell us your thoughts</Button>
+          </View>
         </View>
-        <View style={styles.plantsSection}>
-          <Media direction="row" style={styles.plantTitles}>
-            <Media.Body>
-              <DetailHeader weight="bold">My plants</DetailHeader>
-            </Media.Body>
-            <Media.Item>
-              <Touchable onPress="AddCareGuides" hitSlop={hitSlop}>
-                <Type color="grass" weight="bold">Add a plant</Type>
-              </Touchable>
-            </Media.Item>
-          </Media>
-          <MySavedPlants
-            reloadToggle={reloadPlantsToggle}
-            navigate={props.navigation.navigate}
-            signout={auth.signout}
-          />
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </ScreenWithBottomNav>
   );
 };
 
 PersonalHome.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
+    getParam: PropTypes.func.isRequired,
   }).isRequired,
 };
 
